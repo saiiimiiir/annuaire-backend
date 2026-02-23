cde7ff4eae91eac1d8cfcfe1be2065660d5e9190
Saimir Djoumoi <saimir.djoumoi@gmail.com>
2026-02-23 15:29:58 +0100

    Add PM2 ecosystem config

---
+++ ecosystem.config.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  apps : [{
+    name: 'annuaire-backend',
+    cwd: './backend',
+    script: 'npm',
+    args: 'start',
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 5000
+    },
+    env_development: {
+      NODE_ENV: 'development',
+      PORT: 5000
+    },
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
+    error_file: "./backend/logs/pm2-error.log",
+    out_file: "./backend/logs/pm2-out.log",
+    merge_logs: true,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G'
+  }, {
+    name: 'annuaire-frontend',
+    cwd: './frontend',
+    script: 'npm',
+    args: 'start',
+    env_production: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
+    error_file: "./frontend/logs/pm2-error.log",
+    out_file: "./frontend/logs/pm2-out.log",
+    merge_logs: true,
+    autorestart: true,
+    watch: false
+  }]
+};
